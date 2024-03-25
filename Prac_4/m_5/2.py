b859c501a30ca444a48556c9acbf12491474bbc1
mifoxti <microfoxti@gmail.com>
2024-03-25 10:21:07 +0300

    5.2

--- Prac_4/m_5/2.py
+++ Prac_4/m_5/2.py
@@ -1,73 +1,69 @@
 import tkinter as tk
 
-scale_x = 25
-scale_y = 50
-radius = 10
 
-class Tree:
-    def __init__(self, val, left=None, right=None):
+class TreeNode:
+    def __init__(self, val):
         self.val = val
-        self.left = left
-        self.right = right
-        self.x = 0
-        self.y = 0
+        self.left = None
+        self.right = None
 
-class BinaryTreeVisualizer:
-    def __init__(self):
-        self.current_x = 0
 
-    def visualize(self, tree, depth=0):
-        if tree is None:
-            return
+def create_tree():
+    """
+    Создает пример бинарного дерева.
+    """
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(7)
+    return root
 
-        # Рекурсивно обходим левое поддерево
-        self.visualize(tree.left, depth + 1)
 
-        # Назначаем координаты текущему узлу
-        tree.x = self.current_x
-        tree.y = depth * scale_y
-
-        # Увеличиваем текущую позицию x
-        self.current_x += scale_x
-
-        # Рекурсивно обходим правое поддерево
-        self.visualize(tree.right, depth + 1)
-
-def draw_tree(canvas, tree):
-    if tree is None:
+def tree_coordinates(root, x, y, canvas, level_height=50, level_width=50):
+    """
+    Рекурсивно определяет координаты узлов дерева.
+    """
+    if root is None:
         return
 
-    # Рисуем узел
-    canvas.create_oval(tree.x - radius, tree.y - radius, tree.x + radius, tree.y + radius, fill="white", outline="black")
-    canvas.create_text(tree.x, tree.y, text=str(tree.val))
+    # Рисуем текущий узел
+    canvas.create_oval(x - 10, y - 10, x + 10, y + 10, fill="white", outline="black")
+    canvas.create_text(x, y, text=str(root.val))
 
     # Рисуем связь с левым узлом
-    if tree.left:
-        canvas.create_line(tree.x, tree.y, tree.left.x, tree.left.y, fill="black")
+    if root.left:
+        canvas.create_line(x, y, x - level_width, y + level_height, fill="black")
+        tree_coordinates(root.left, x - level_width, y + level_height, canvas, level_height, level_width / 2)
 
     # Рисуем связь с правым узлом
-    if tree.right:
-        canvas.create_line(tree.x, tree.y, tree.right.x, tree.right.y, fill="black")
+    if root.right:
+        canvas.create_line(x, y, x + level_width, y + level_height, fill="black")
+        tree_coordinates(root.right, x + level_width, y + level_height, canvas, level_height, level_width / 2)
 
-# Пример использования:
-tree_2 = Tree(2, Tree(3, Tree(4), Tree(5)), Tree(6, Tree(7)))
-tree_8 = Tree(8, Tree(9, Tree(10), Tree(11, Tree(12), Tree(13))), Tree(14))
-tree = Tree(1, tree_2, tree_8)
 
-visualizer = BinaryTreeVisualizer()
-visualizer.visualize(tree)
+def visualize_tree(root):
+    """
+    Визуализирует бинарное дерево.
+    """
+    root_window = tk.Tk()
+    root_window.title("Binary Tree Visualization")
 
-# Создаем окно Tkinter
-root = tk.Tk()
-root.title("Binary Tree Visualization")
+    canvas_width = 800
+    canvas_height = 600
+    canvas = tk.Canvas(root_window, width=canvas_width, height=canvas_height)
+    canvas.pack()
 
-# Создаем холст для рисования дерева
-canvas_width = visualizer.current_x + scale_x
-canvas_height = max(node.y for node in [tree, tree_2, tree_8]) + scale_y
-canvas = tk.Canvas(root, width=canvas_width, height=canvas_height)
-canvas.pack()
+    # Рисуем дерево
+    tree_coordinates(root, canvas_width / 2, 50, canvas)
 
-# Рисуем дерево
-draw_tree(canvas, tree)
+    root_window.mainloop()
 
-root.mainloop()
+
+# Создаем пример бинарного дерева
+root = create_tree()
+
+# Визуализируем дерево
+visualize_tree(root)
