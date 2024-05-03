a1dfe56dfd3b145d3d34b281cfd884c475b9ac6b
mifoxti <microfoxti@gmail.com>
2024-05-03 17:51:56 +0300

    +1 миска рис

--- KIS/12/v10.py
+++ KIS/12/v10.py
@@ -1,6 +1,8 @@
+from pprint import pprint
 from struct import *
 
-# НЕ РАБОТАЕТ
+
+# РАБОТАЕТ
 def parse(buffer, offset, type, order='>'):
     pattern = {
         'float': 'f',
@@ -22,7 +24,7 @@ def parse(buffer, offset, type, order='>'):
 
 def parse_a(buffer, offset):
     offset = 4
-    a1_offset, offset = parse(buffer, offset, 'uint16')
+    a1_offset, offset = parse(buffer, offset, 'uint32')
     a1, _ = parse_b(buffer, a1_offset)
     a2 = []
     array_size, offset = parse(buffer, offset, 'uint16')
@@ -36,7 +38,10 @@ def parse_a(buffer, offset):
 
 
 def parse_b(buffer, offset):
-    b1, offset = parse(buffer, offset, 'char', '>3')
+    b1 = ''
+    for _ in range(3):
+        val, offset = parse(buffer, offset, 'char')
+        b1 += (val.decode())
     b2, offset = parse(buffer, offset, 'int32')
     b3, offset = parse(buffer, offset, 'int64')
     b4, offset = parse(buffer, offset, 'int16')
@@ -63,8 +68,14 @@ def parse_c(buffer, offset):
 
 def parse_d(buffer, offset):
     d1, offset = parse(buffer, offset, 'int16')
-    d2, offset = parse(buffer, offset, 'float', '>8')
-    d3, offset = parse(buffer, offset, 'uint16', '>8')
+    d2 = []
+    for _ in range(8):
+        val, offset = parse(buffer, offset, 'float')
+        d2.append(val)
+    d3 = []
+    for _ in range(8):
+        val, offset = parse(buffer, offset, 'uint16')
+        d3.append(val)
     return {'D1': d1, 'D2': d2, 'D3': d3}, offset
 
 
@@ -73,10 +84,10 @@ def main(data):
     return result
 
 
-print(main((b'\xedWKW\x00\x00\x00\x1c\x00\x02\x00\x9dU\xe51\xd5\x1bp!\x00\x03\x00\r\x92'
-            b"%'\x7f\xf1dki\xce\x86\x92\x16H\x84\x8d\x10|\x94T\x14\xc0\x97\xa3\x91\xa3"
-            b'\xc7\xd6\x00\xfd\xa9\x00\x00\x00\x13\xe1,\xbfB\xcbv\xbf0\x81\xfe?2\xd1\xb9?'
-            b'D\xa8\x08=\xe9p0\xbd\xf9v\x1d\xbd\xe8\xe8\xb7\xbe\xd42\xa3<\xb8\x0c\xa1\x90'
-            b'w\x84\x9c\xd5\x14\xe8\xa6\xdb\x12\x98\xc7\xecN\xbf\x0e7X\xbd\xc9\xe6'
-            b'V?\x16\x99\xf4>(\xef\xf9>\xdcrm\xbf1!A?)\xa8E?\rp\x95\xc0\x9c\xe5\xf4 <\xc5'
-            b"\x85\x87'^\xd6\xe1\xec\x931\x009\x00k")))
+pprint(main((b'\xedWKW\x00\x00\x00\x1c\x00\x02\x00\x9dU\xe51\xd5\x1bp!\x00\x03\x00\r\x92'
+             b"%'\x7f\xf1dki\xce\x86\x92\x16H\x84\x8d\x10|\x94T\x14\xc0\x97\xa3\x91\xa3"
+             b'\xc7\xd6\x00\xfd\xa9\x00\x00\x00\x13\xe1,\xbfB\xcbv\xbf0\x81\xfe?2\xd1\xb9?'
+             b'D\xa8\x08=\xe9p0\xbd\xf9v\x1d\xbd\xe8\xe8\xb7\xbe\xd42\xa3<\xb8\x0c\xa1\x90'
+             b'w\x84\x9c\xd5\x14\xe8\xa6\xdb\x12\x98\xc7\xecN\xbf\x0e7X\xbd\xc9\xe6'
+             b'V?\x16\x99\xf4>(\xef\xf9>\xdcrm\xbf1!A?)\xa8E?\rp\x95\xc0\x9c\xe5\xf4 <\xc5'
+             b"\x85\x87'^\xd6\xe1\xec\x931\x009\x00k")))
